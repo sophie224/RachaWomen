c66f8e1a8df2afdc5e7c291117ade6ff6202b8a5
sophiko pkhikidze <33790486+sophie224@users.noreply.github.com>
2023-12-31 18:05:05 +0400

    add info

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 // setTimeout(() => {
 //     document.getElementById('loader').style.display='none'          
 // }, 1500);
+console.log('all',document.querySelectorAll('.hidden_container'))
 function showPhoto(clickedId) {
+    const allHiddenContainer = document.querySelectorAll('.hidden_container')
+    for (let i = 0; i < allHiddenContainer.length; i++) {
+        allHiddenContainer[i].style.display = "none";
+    }
     var historyDiv = document.getElementById('story_' + clickedId)
+    document.querySelector('.full_story_container').style.display = "flex";
     if (historyDiv) {
         historyDiv.style.display = "block";
-        document.getElementById('mainContainer').style.display='none'
+        
     }
 }
 function hidePhoto(id) {
     document.getElementById(id).style.display='none'
-    document.getElementById('mainContainer').style.display='block'
+    document.querySelector('.full_story_container').style.display = "none";
+
+    const allHiddenContainer = document.querySelectorAll('.hidden_container')
+    for (let i = 0; i < allHiddenContainer.length; i++) {
+        allHiddenContainer[i].style.display = "grid";
+    }
     
 }
