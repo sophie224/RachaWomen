26e3a50eb5bb91b24d7758130316de24ccb43ea8
sophiko pkhikidze <33790486+sophie224@users.noreply.github.com>
2024-01-03 13:52:57 +0400

    add new info

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-// setTimeout(() => {
-//     document.getElementById('loader').style.display='none'          
-// }, 1500);
-console.log('all',document.querySelectorAll('.hidden_container'))
 function showPhoto(clickedId) {
+    scrollToTop()
     const allHiddenContainer = document.querySelectorAll('.hidden_container')
     for (let i = 0; i < allHiddenContainer.length; i++) {
         allHiddenContainer[i].style.display = "none";
     }
-    var historyDiv = document.getElementById('story_' + clickedId)
+    const historyDiv = document.getElementById('story_' + clickedId)
     document.querySelector('.full_story_container').style.display = "flex";
     if (historyDiv) {
         historyDiv.style.display = "block";
-        
     }
 }
 function hidePhoto(id) {
-    document.getElementById(id).style.display='none'
+    document.getElementById(id).style.display = 'none'
     document.querySelector('.full_story_container').style.display = "none";
-
     const allHiddenContainer = document.querySelectorAll('.hidden_container')
     for (let i = 0; i < allHiddenContainer.length; i++) {
         allHiddenContainer[i].style.display = "grid";
     }
-    
 }
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'auto'
+    });
+}
+
+const buttonsWithDataId = document.querySelectorAll('[data-id="backArrow"]');
+const mainContainer = document.getElementById('mainContainer')
+
+buttonsWithDataId.forEach(function(button) {
+    button.addEventListener('click', function() {
+        mainContainer.scrollIntoView({ behavior: 'smooth' });
+    });
+});
